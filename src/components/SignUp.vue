82bfbf9d37ce9ea2cb9cf78c1fcee6806e441a30
belladev250 <lissadev17@gmail.com>
2024-07-21 15:29:03 +0200

    tailwind

---
+++ src/components/SignUp.vue
@@ -0,0 +1,10 @@
+<template>
+    <h1 class="text-blue-500 text-lg  "> Sign up</h1>
+     <p class="text-lg">Its dine ease</p>
+</template>
+
+<script>
+   export default{
+    name:'SignUp'
+   }
+</script>
